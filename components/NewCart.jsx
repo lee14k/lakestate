62fdc7fe4b7cb5bb2d9179c2b627bd159e93e28f
lee14k <kmkovach@mtu.edu>
2024-01-16 14:06:55 -0500

    trying to figure out how products work

--- components/NewCart.jsx
+++ components/NewCart.jsx
@@ -38,21 +38,7 @@ export default function NewCart() {
 
                 <form className="mx-auto max-w-2xl px-4">
                     <ul role="list" className="divide-y divide-gray-200">
-                        {products.map((product) => (
-                            <li key={product.id} className="flex items-center py-6">
-                                <img
-                                    src={product.imageSrc}
-                                    alt={product.imageAlt}
-                                    className="h-16 w-16 flex-none rounded-md border border-gray-200"
-                                />
-                                <div className="ml-4 flex-auto">
-                                    <h3 className="font-medium text-gray-900">
-                                        <a href={product.href}>{product.name}</a>
-                                    </h3>
-                                    <p className="text-gray-500">{product.color}</p>
-                                </div>
-                            </li>
-                        ))}
+               
                     </ul>
 
                     <button
