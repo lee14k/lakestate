d7d554147f00fedf42b49ecc1aec133fb1d7b86d
lee14k <kmkovach@mtu.edu>
2024-01-18 16:37:18 -0500

    trying to get icon next to shopping bag change

--- components/NewCart.jsx
+++ components/NewCart.jsx
@@ -22,7 +22,9 @@ export default function NewCart() {
                 className="h-6 w-6 flex-shrink-0 text-gray-400 group-hover:text-gray-500"
                 aria-hidden="true"
             />
-            <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">0</span>
+            <span className="ml-2 text-sm font-medium text-gray-700 group-hover:text-gray-800">
+                {cartItems.length}
+            </span>
             <span className="sr-only">items in cart, view bag</span>
         </Popover.Button>
         <Transition
