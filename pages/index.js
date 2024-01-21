92f7a0924c93986c0788211c73bac8b4fb79e2df
lee14k <kmkovach@mtu.edu>
2024-01-21 01:39:27 -0500

    fixing first cta linking

--- pages/index.js
+++ pages/index.js
@@ -34,13 +34,15 @@ export default function Home() {
           impact on our community.
         </p>
     
-        <Link href="/Donate">
           <div className="mb-16 my-8">
             <span className="z-500 rounded-md bg-sky-900  px-10 py-6 text-xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mb-48">
-              <span>Support our Mission</span>
+            <Link href="/Donate">
+
+            <span>Support our Mission</span>
+            </Link>
+
             </span>
           </div>
-        </Link>
       </div>
 
       <StoreCTA />
