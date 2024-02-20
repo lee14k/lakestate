495c04ca86de8c204b4cd95a8b27e11cc4704ac5
lee14k <kmkovach@mtu.edu>
2024-02-20 18:24:29 -0500

    mobile optimization

--- components/ServiceGrid.jsx
+++ components/ServiceGrid.jsx
@@ -11,10 +11,10 @@ export default function ServiceGrid({
 <div className="serviceswrapper">
       {services.map((service, index) => (
         <div key={index} className={`serve flex ${index % 2 === 0 ? 'flex-row ' : 'flex-row-reverse bg-sky-900 text-white'} items-center`}>
-            <div className="service-image">
+            <div className="service-image w-1/2">
             {service.backgroundImage && <img src= {service.backgroundImage} style={{ width: '100%', height: 'auto' }}/>}
           </div>
-          <div className="service-content w-1/2 flex flex-col justify-center items-start px-4">
+          <div className="service-content sm:w-48 w-1/2  flex flex-col justify-center items-start px-4">
             {service.header && <h2 className="text-4xl mb-4">{service.header}</h2>}
             {service.para && <p className="mb-4">{service.para}</p>}
             {service.buttonText && (
