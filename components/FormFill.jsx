f787e4ea281c522339b120e1b571b89377e42978
lee14k <kmkovach@mtu.edu>
2024-02-20 09:31:13 -0500

    filling out form section and picture adjustments

--- components/FormFill.jsx
+++ components/FormFill.jsx
@@ -4,66 +4,20 @@ export default function FormFill() {
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="grid grid-cols-1 items-center gap-x-8 gap-y-16 lg:grid-cols-2">
             <div className="mx-auto w-full max-w-xl lg:mx-0">
-              <h2 className="text-3xl font-bold tracking-tight text-gray-900">Trusted by the most innovative teams</h2>
+              <h2 className="text-3xl font-bold tracking-tight text-gray-900">Helpful Links and Forms</h2>
               <p className="mt-6 text-lg leading-8 text-gray-600">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et, egestas tempus tellus etiam sed. Quam a
-                scelerisque amet ullamcorper eu enim et fermentum, augue.
+        Click on the links below to access the forms or assistance you need. If you have any questions, please contact us.
               </p>
-              <div className="mt-8 flex items-center gap-x-6">
-                <a
-                  href="#"
-                  className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                >
-                  Create account
-                </a>
-                <a href="#" className="text-sm font-semibold text-gray-900">
-                  Contact us <span aria-hidden="true">&rarr;</span>
-                </a>
-              </div>
+      
             </div>
-            <div className="mx-auto grid w-full max-w-xl grid-cols-2 items-center gap-y-12 sm:gap-y-14 lg:mx-0 lg:max-w-none lg:pl-8">
-              <img
-                className="max-h-12 w-full object-contain object-left"
-                src="https://tailwindui.com/img/logos/tuple-logo-gray-900.svg"
-                alt="Tuple"
-                width={105}
-                height={48}
-              />
-              <img
-                className="max-h-12 w-full object-contain object-left"
-                src="https://tailwindui.com/img/logos/reform-logo-gray-900.svg"
-                alt="Reform"
-                width={104}
-                height={48}
-              />
-              <img
-                className="max-h-12 w-full object-contain object-left"
-                src="https://tailwindui.com/img/logos/savvycal-logo-gray-900.svg"
-                alt="SavvyCal"
-                width={140}
-                height={48}
-              />
-              <img
-                className="max-h-12 w-full object-contain object-left"
-                src="https://tailwindui.com/img/logos/laravel-logo-gray-900.svg"
-                alt="Laravel"
-                width={136}
-                height={48}
-              />
-              <img
-                className="max-h-12 w-full object-contain object-left"
-                src="https://tailwindui.com/img/logos/transistor-logo-gray-900.svg"
-                alt="Transistor"
-                width={158}
-                height={48}
-              />
-              <img
-                className="max-h-12 w-full object-contain object-left"
-                src="https://tailwindui.com/img/logos/statamic-logo-gray-900.svg"
-                alt="Statamic"
-                width={147}
-                height={48}
-              />
+            <div className="mx-auto grid w-full max-w-xl grid-cols-3 items-center gap-6">
+                <button className="max-h-26 w-full object-contain object-left rounded-md bg-orange-400  px-10 py-6 text-xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Pathways Community Mental Health</button>
+                <button className="max-h-26 w-full object-contain object-left rounded-md bg-orange-400  px-10 py-6 text-xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 "> Michigan Rehabilitation Services (MRS)</button>
+                <button className="max-h-26 w-full object-contain object-left rounded-md bg-orange-400  px-10 py-6 text-xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"> Respite Form</button>
+                <button className="max-h-26 w-full object-contain object-left rounded-md bg-orange-400  px-10 py-6 text-xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"> Hiawatha Respite Form</button>
+                <button className="max-h-26 w-full object-contain object-left rounded-md bg-orange-400  px-10 py-6 text-xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 "> Employment Application</button>
+     
+          
             </div>
           </div>
         </div>
--- pages/VocationalTraining.js
+++ pages/VocationalTraining.js
@@ -12,29 +12,29 @@ const VocationalTraining = () => {
           headerOne="Quality Services for Hire"
           headerTwo="Confidential Shredding and Document Destruction"
         headerThree="Janitorial Services"
-        bgImage1='/cleaner.jpg'
+        bgImage1='/vchead.jpg'
         header="Vocational Training"
           
         services={[
           {
             header: "Individualized Assessments",
             para: "We conduct comprehensive assessments to identify each individual's strengths, abilities, interests, and support needs.",
-            backgroundImage: "/cleaner.jpg"
+            backgroundImage: "/vcone.jpg"
           },
           {
             header: "Skill Development",
             para: "We offer specialized training in a wide range of vocational skills tailored to the individual's interests and capabilities, such as, job-specific tasks, communication skills, and workplace etiquette.",
-            backgroundImage: "/shreds.jpg"
+            backgroundImage: "/vctwo.jpg"
           },
           {
             header: "Job Placement Services",
             para: "We provide assistance with job searches, resume building, interview preparation, and connecting individuals with inclusive employers who value inclusion and diversity..",
-            backgroundImage: "/shreds.jpg"
+            backgroundImage: "/vcthree.jpg"
           },
           {
             header: "Job Coaching and Support",
             para: "We offer ongoing support and job coaching to help individuals succeed in their chosen roles, including on-the-job training, and problem-solving assistance.",
-            backgroundImage: "/shreds.jpg"
+            backgroundImage: "/vcfour.jpg"
           },
           {
             header: "Social and Life Skills Training",
