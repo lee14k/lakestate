47f390a2f6228b02550fa319b428447f169e5c2a
lee14k <kmkovach@mtu.edu>
2024-03-07 00:23:44 -0500

    trying to fix description margins

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,64 +1,66 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
-import styles from '../../styles/Shop.module.css';
-import Navbar from '@/components/Navbar';
-import Footer from '@/components/Footer';
-import NewCart from '@/components/NewCart';
-import { CartProvider } from '../../context/CartContext'; // Update the path to CartContext
-import BuyButton from '@/components/BuyButton';
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import styles from '../../styles/Shop.module.css'
+import Navbar from '@/components/Navbar'
+import Footer from '@/components/Footer'
+import NewCart from '@/components/NewCart'
+import { CartProvider } from '../../context/CartContext' // Update the path to CartContext
+import BuyButton from '@/components/BuyButton'
 export async function getStaticPaths() {
-  const url = new URL('https://lakestate.vercel.app');
-  url.pathname = '/api/products';
+  const url = new URL('https://lakestate.vercel.app')
+  url.pathname = '/api/products'
 
-  const res = await fetch(url.toString());
+  const res = await fetch(url.toString())
 
   // Check for a successful response
   if (!res.ok) {
-    console.error('Failed to fetch products:', res);
+    console.error('Failed to fetch products:', res)
     // Return an empty paths array in case of an error
-    return { paths: [], fallback: false };
+    return { paths: [], fallback: false }
   }
 
-  const data = await res.json();
+  const data = await res.json()
 
   // Map data to paths
   const paths = data.products.edges.map(({ node }) => ({
     params: { slug: node.handle },
-  }));
-  
+  }))
 
   return {
     paths,
     fallback: true,
-  };
+  }
 }
 export async function getStaticProps({ params }) {
-  const url = new URL('https://lakestate.vercel.app');
-  url.pathname = '/api/products';
+  const url = new URL('https://lakestate.vercel.app')
+  url.pathname = '/api/products'
 
-  const res = await fetch(url.toString());
+  const res = await fetch(url.toString())
 
   if (!res.ok) {
-    return { props: {} };
+    return { props: {} }
   }
 
-  const data = await res.json();
-  const defaultImageSrc = '/lakestatelogo.png';
+  const data = await res.json()
+  const defaultImageSrc = '/lakestatelogo.png'
 
- 
-  let product = null; // Define product variable
+  let product = null // Define product variable
 
   data.products.edges.forEach(({ node }) => {
-    if (node.handle === params.slug) { // Find the product with matching slug
-      const imageSrc = node.images.edges.length > 0 ? node.images.edges[0].node.src : defaultImageSrc;
+    if (node.handle === params.slug) {
+      // Find the product with matching slug
+      const imageSrc =
+        node.images.edges.length > 0
+          ? node.images.edges[0].node.src
+          : defaultImageSrc
 
       const variants = node.variants.edges.map(({ node }) => ({
         id: node.id,
         title: node.title,
         price: node.priceV2.amount,
         quantityAvailable: node.quantityAvailable,
-      }));
+      }))
 
       product = {
         id: node.id,
@@ -69,75 +71,66 @@ export async function getStaticProps({ params }) {
         price: node.variants.edges[0]?.node.priceV2.amount,
         slug: node.handle,
         variants,
-      };
+      }
     }
-  });
-
+  })
 
   if (!product) {
-    console.log("No product found for slug:", params.slug);
-    return { props: { product: null }, revalidate: 10 };
+    console.log('No product found for slug:', params.slug)
+    return { props: { product: null }, revalidate: 10 }
   }
 
   return {
     props: { product },
     revalidate: 10,
-  };
+  }
 }
 
 function Product({ slug, imageSrc, imageAlt, title, description, price }) {
   const formattedPrice = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
-  });
+  })
 
   return (
     <div>
-    <div className="sm:flex mx-12 my-24">
-      <a href={`/product/${slug}`}>
-        <Image src={imageSrc} alt={imageAlt} width={600} height={600} />
-      </a>
-      <div className="flex flex-col items-center mr-12">
-        <div className="flex flex-col">
-      <h2 className="text-2xl">{title}</h2>
-      <p className>{formattedPrice.format(price)}</p>
-      <p >{description}</p>
-      <BuyButton product={Product} />
-
-      </div>
-
-
+      <div className="sm:flex mx-12 my-24 gap-12">
+        <a href={`/product/${slug}`}>
+          <Image src={imageSrc} alt={imageAlt} width={600} height={600} />
+        </a>
+        <div className="flex flex-col items-center mr-12">
+          <div className="flex flex-col">
+            <h2 className="text-2xl">{title}</h2>
+            <p className>{formattedPrice.format(price)}</p>
+            <p className="sm:mx-24">{description}</p>
+            <BuyButton product={Product} />
+          </div>
+        </div>
       </div>
     </div>
-    </div>
-  );
+  )
 }
 
 export default function ProductPage({ product }) {
-
   return (
     <div className>
-          <CartProvider>
+      <CartProvider>
+        <Head>
+          <title>Lakestate Industries</title>
+        </Head>
 
-      <Head>
-        <title>Lakestate Industries</title>
+        <main>
+          <Navbar />
 
-      </Head>
+          <Link href="/Shop">&larr; back to the store</Link>
 
-      <main >
-        <Navbar/>
-
-        <Link href="/Shop">&larr; back to the store</Link>
-
-        <div >
-          <NewCart/>
-          <Product {...product} />
-          
-        </div>
-      </main>
+          <div>
+            <NewCart />
+            <Product {...product} />
+          </div>
+        </main>
       </CartProvider>
-            <Footer/>
-
+      <Footer />
     </div>
-  );
+  )
 }
