dea4828bda7d024089a4343ff1134d21920fee7f
lee14k <kmkovach@mtu.edu>
2024-03-07 09:17:45 -0500

    copy stuff

--- components/StoreCTA.jsx
+++ components/StoreCTA.jsx
@@ -1,11 +1,11 @@
-import { Handlee } from "next/font/google";
-import Link from "next/link";
-import PictureGrid from "./PictureGrid";
+import { Handlee } from 'next/font/google'
+import Link from 'next/link'
+import PictureGrid from './PictureGrid'
 const handlee = Handlee({
-  weight: ["400"],
-  display: "swap",
-  subsets: ["latin"],
-});
+  weight: ['400'],
+  display: 'swap',
+  subsets: ['latin'],
+})
 export default function StoreCTA() {
   return (
     <div className="storehome flex flex-col justify-center items-center">
@@ -13,30 +13,25 @@ export default function StoreCTA() {
         <div className>
           <div className={handlee.className}>
             <div className="flex flex-col justify-center items-center">
-            <h1 className="handcraftheadline text-4xl sm:text-6xl my-12 lakerhead">
-              Handcrafted Goods
-            </h1>
+              <h1 className="handcraftheadline text-4xl sm:text-6xl my-12 lakerhead">
+                Michigan Made Products{' '}
+              </h1>
             </div>
-            </div>
-            <p className="sm:mx-60 mb-10 text-xl sm:text-2xl text-white text-center">
-                Our products are made with love by our students and staff. We
-                offer a wide range of products, from woodwork to sewing, and
-                everything in between. We also offer custom orders. If you have
-                something in mind, we can make it happen.
-            </p>
-            
-          
+          </div>
+          <p className="sm:mx-60 mb-10 text-xl sm:text-2xl text-white text-center">
+          Our products are meticulously crafted with care and dedication, thanks to the collaboration between our valued clients and our dedicated staff. This partnership ensures that each item is made with the utmost attentions to detail and quality. If you have a project, idea or need help with a product for an event, please contact us for more details.
+
+          </p>
         </div>
       </div>
       <PictureGrid />
-          <div className="mb-48">
-            <Link href="/Shop">
-         <span className="rounded-md bg-orange-400 px-10 py-6 text-2xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-              <span>Shop now</span>
-              </span>
-              </Link>
-              
-          </div>
+      <div className="mb-48">
+        <Link href="/Shop">
+          <span className="rounded-md bg-orange-400 px-10 py-6 text-2xl font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            <span>Shop now</span>
+          </span>
+        </Link>
+      </div>
     </div>
-  );
+  )
 }
