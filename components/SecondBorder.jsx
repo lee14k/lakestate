b59558fd4f0c4332df20b6ceb9ac51d15e417281
lee14k <kmkovach@mtu.edu>
2024-03-11 01:13:02 -0400

    screwing around with mobile

--- components/SecondBorder.jsx
+++ components/SecondBorder.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 
 export default function SecondBorder(props) {
   // Container style includes the background image
@@ -8,17 +8,27 @@ export default function SecondBorder(props) {
     height: '1000px', // Adjust the height as necessary
     padding: '0px',
     position: 'relative', // Ensure we can position children absolutely within
-  };
+  }
 
   return (
     <div className="headerscontainer" style={containerStyle}>
       {/* Positioned Header */}
       <div className="header-content flex justify-center items-center h-full">
-        <h1 className="text-white text-5xl sm:text-8xl lakerhead ">{props.header}</h1>
+        <h1 className="text-white text-5xl sm:text-8xl lakerhead ">
+          {props.header}
+        </h1>
       </div>
-      
+
       {/* Waves Overlay */}
-      <div className="headerstwo" style={{ position: 'absolute', bottom: '-40px', left: '0', right: '0' }}></div>
+      <div
+        className="headerstwo"
+        style={{
+          position: 'absolute',
+          bottom: '-120px',
+          left: '0',
+          right: '0',
+        }}
+      ></div>
     </div>
-  );
+  )
 }
