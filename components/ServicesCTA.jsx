9ca89d5a339c38511e5fc1b8666c40e8915a110a
lee14k <kmkovach@mtu.edu>
2024-06-25 00:59:45 -0400

    image compression and logos

--- components/ServicesCTA.jsx
+++ components/ServicesCTA.jsx
@@ -8,7 +8,7 @@ export default function ServicesCTA({ topImage1Src, topImage2Src }) {
       >
         <div className="gradient-overlay flex flex-col justify-center items-center">
           <div className="flex flex-col justify-center items-center mt-4">
-            <h1 className="text-white text-xl mb-6">
+            <h1 className="text-white text-xl mb-6 bg-black bg-opacity-50">
               Interested in our vocational training?
             </h1>
             <Link href="/VocationalTraining">
@@ -26,7 +26,7 @@ export default function ServicesCTA({ topImage1Src, topImage2Src }) {
       >
         <div className="gradient-overlay flex flex-col justify-center items-center">
           <div className="flex flex-col justify-center items-center mt-4">
-            <h1 className="text-white text-xl mb-6">Looking to hire us?</h1>
+            <h1 className="text-white text-xl mb-6 bg-black bg-opacity-50">Looking to hire us?</h1>
             <Link href="/HireUs">
               <button className="rounded-md bg-orange-400 px-6 py-2 text-l font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                 Learn More
