cac8ae631102d36b97ec79f2736bedb69a42c1f7
lee14k <kmkovach@mtu.edu>
2024-07-09 13:51:44 -0400

    trying to make the cart work better

--- context/CartContext.js
+++ context/CartContext.js
@@ -4,17 +4,23 @@ const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
+  const [isCartOpen, setIsCartOpen] = useState(false);
 
   const addToCart = (product) => {
-    setCartItems(currentItems => [...currentItems, product]);
+    setCartItems((currentItems) => [...currentItems, product]);
+    setIsCartOpen(true);  // Open the cart when an item is added
   };
 
   const removeFromCart = (productId) => {
-    setCartItems(currentItems => currentItems.filter(item => item.id !== productId));
+    setCartItems((currentItems) => currentItems.filter((item) => item.id !== productId));
+  };
+
+  const toggleCart = () => {
+    setIsCartOpen((prevState) => !prevState);
   };
 
   return (
-    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart }}>
+    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart, isCartOpen, toggleCart }}>
       {children}
     </CartContext.Provider>
   );
